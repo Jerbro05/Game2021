8763610383940a2e56e0cadaa70fbd91ce53e9ec
jeremy <jerbro05@kingshigh.school.nz>
2021-06-08 14:55:15 +1200

    enemy gravity

--- dte game/game/enemy.gd
+++ dte game/game/enemy.gd
@@ -1,9 +1,11 @@
 extends KinematicBody2D
 
+export (int) var gravity = 5500
 class_name Enemy
 onready var player = get_tree().get_nodes_in_group("Player")
 var state = "chasing"  
-var speed = 500
+var speed = 250
+var velocity = Vector2.ZERO
 
 func _ready():
 	player = player[0]
@@ -16,14 +18,32 @@ func _process(delta):
 	else:
 		patrol()
 
-
 func idle():
 	pass
-
 
 func patrol():
 	pass
 
 func chasing(delta):
+	velocity.y += gravity * delta
+
 	var player_position = player.global_position
+	if position.x < player_position.x :
+		pass
+		velocity.x = speed * delta
+	else:
+		pass
+		velocity.x = -speed * delta
+	
+	if velocity.x != 0 :
+		$AnimationPlayer.play("walk")
+		if velocity.x < 0: 
+			$Sprite.flip_h = true
+		else: 
+			$Sprite.flip_h = false
+	else:
+		$AnimationPlayer.play("idle") 
+
+		
+	velocity = move_and_slide(velocity, Vector2.UP)
 	position = position.move_toward(player_position, speed * delta)
