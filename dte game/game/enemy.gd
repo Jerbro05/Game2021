bef0bba5a09602fbfdcd777f6ea6b88e77779223
jeremy <jerbro05@kingshigh.school.nz>
2021-06-10 09:54:13 +1200

    nothing

--- dte game/game/enemy.gd
+++ dte game/game/enemy.gd
@@ -6,9 +6,11 @@ onready var player = get_tree().get_nodes_in_group("Player")
 var state = "chasing"  
 var speed = 250
 var velocity = Vector2.ZERO
+var target
 
 func _ready():
 	player = player[0]
+	
 	
 func _process(delta):
 	if state == "idle":
@@ -44,6 +46,21 @@ func chasing(delta):
 	else:
 		$AnimationPlayer.play("idle") 
 
-		
-	velocity = move_and_slide(velocity, Vector2.UP)
-	position = position.move_toward(player_position, speed * delta)
+	if target:
+		velocity = move_and_slide(velocity, Vector2.UP)
+		position = position.move_toward(player_position, speed * delta)
+
+
+
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("Player"):
+		target = body
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("Player"):
+		target = null
+	
+
+
+func _on_Area2D_area_shape_exited(area_id, area, area_shape, local_shape):
+	pass # Replace with function body.
