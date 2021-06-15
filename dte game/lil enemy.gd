ab3bb50b3be57bbf94b000b25d47bb8e982aef8a
jeremy <jerbro05@kingshigh.school.nz>
2021-06-15 13:24:40 +1200

    lil man code
    lil man runs but doesnt anamater properly

--- dte game/lil enemy.gd
+++ dte game/lil enemy.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export (int) var gravity = 5500
-class_name Enemy
+class_name lil 
 onready var player = get_tree().get_nodes_in_group("Player")
 var state = "idle" 
 var speed = 250
@@ -10,21 +10,18 @@ var target
 
 func _ready():
 	player = player[0]
-	
-	
+
 func _process(delta):
 	if state == "idle":
 		idle()
 	elif state == "chasing":
 		chasing(delta)
 
-
 func idle():
 	$AnimationPlayer.play("idle") 
 
 func chasing(delta):
 	velocity.y += gravity * delta
-
 	var player_position = player.global_position
 	if position.x < player_position.x :
 		pass
@@ -34,19 +31,17 @@ func chasing(delta):
 		velocity.x = -speed * delta
 	
 	if velocity.x != 0 :
-		$AnimationPlayer.play("walk")
+		$AnimationPlayer.play("run")
 		if velocity.x < 0: 
 			$Sprite.flip_h = true
 		else: 
 			$Sprite.flip_h = false
 	else:
 		$AnimationPlayer.play("idle") 
-
+		
 	if target:
 		velocity = move_and_slide(velocity, Vector2.UP)
 		position = position.move_toward(player_position, speed * delta)
-
-
 
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("Player"):
@@ -57,8 +52,10 @@ func _on_Area2D_body_exited(body):
 	if body.is_in_group("Player"):
 		target = null
 		state = "idle"
-	
-
 
 func _on_Area2D_area_shape_exited(area_id, area, area_shape, local_shape):
-	pass # Replace with function body.
+	pass 
+
+func _on_Area2D_area_shape_entered(area_id, area, area_shape, local_shape):
+	pass 
+
